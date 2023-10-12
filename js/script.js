fe7c2362dde5e5a990243968bfd2b9cee29194d1
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-12 17:17:48 +0200

    html Email

--- js/script.js
+++ js/script.js
@@ -32,7 +32,10 @@ if (user > pc) {
 })
 
 
-
+//Mail
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
 
 
 
