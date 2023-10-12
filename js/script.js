f422fe1608faa86bb1cc746c65cbe80c7c52d423
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-12 16:42:10 +0200

    math.random user + pc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 // Gioco dei dadi 
-const bottone = document.getElementById ('bottone');
-
-bottone.addEventListener('click', function (){
-
-})
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
+const Bottone = document.getElementById("bottone");
+let min = 1;
+let max = 6;
 
-//random
-function getRndInteger(min, max){
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+Bottone.addEventListener("click", function(){
+  
+ 
+const user = (Math.random() * (max - min + 1) + min);
+const userNumber = document.querySelector(".userNumber");
+userNumber.innerHTML =" "+ user;
+
+
+const pc = (Math.random() * (max - min + 1) + min);
+const pcNumber = document.querySelector(".pcNumber");
+pcNumber.innerHTML = " "+ pc;
+
 }
+)
